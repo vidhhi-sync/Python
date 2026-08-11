95e7adc48982e1463ff9e085461b8f34cac3827d
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-11 12:59:29 +0530

    Adding Input From user

---
+++ Input-in-python.py
@@ -0,0 +1,16 @@
+name =input("Enter your name:")
+print(name)
+print(type(name))
+
+age = int(input("Enter your age:"))
+print(age)
+print(type(age))
+
+#printing student details
+Name = input("Enter your name:")
+Age = int(input("Enter your age:"))
+Marks = float(input("Enter your marks:"))
+
+print("Welcome,", Name)
+print("Your age is :",Age)
+print("Marks you obtained are:",Marks)
