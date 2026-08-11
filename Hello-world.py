5392f169dd642983a25e2664bbdc87ff49173589
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-11 12:57:17 +0530

    Adding Introduction to Python

---
+++ Hello-world.py
@@ -0,0 +1,36 @@
+#First program
+#printing hello world using print statement
+print("Hello world")
+
+#Comment statement
+"""Hi there, I am learning python"""
+
+#printing multiple statements in one line
+print("Hello", "Welcome vidhi")
+
+#printing direct numbers
+print(23)
+print(23+28)
+
+#variables
+#here the name is assigned a value or assigned a memory
+name = "vidhi"
+print(name)
+print("My name is :",name)
+name1 = name
+print("My name is:",name1)
+age = 12
+
+#detecting the type of value stored in a variable using type() 
+print(type(name))
+print(type(age))
+
+#printing sum of 2 numbers
+a = 2
+b = 4
+print(a+b) 
+c = a+b 
+print(c)
+
+
+
