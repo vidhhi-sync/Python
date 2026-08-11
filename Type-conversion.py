99c81c32e1dfc28010905d9f8065f1ca47689569
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-11 12:58:59 +0530

    Adding Type conversion and casting

---
+++ Type-conversion.py
@@ -0,0 +1,21 @@
+#Type Conversion
+#Done automatically
+
+a = 1
+b = 2.3
+print(a+b)
+
+"""In this program, the output we obtain is 3.3 which signifies that the 
+python interpreter itself/automatically converted the type of int to float (As
+the float is at more priority than integer) to add the above two numbers."""
+
+#Type Casting
+#it is done manually means forcefully converting datatypes into one another
+
+c = 23
+d = str(c)
+print(type(c))
+print(type(d))
+
+"""In this program, the output signifies that the variable c is forcefully converted from 
+integer type to a string type."""
