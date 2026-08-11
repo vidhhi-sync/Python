a1aedb941d8beb5922ce33379fb441e5b65cc5a3
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-11 13:00:34 +0530

    Adding Practice Questions Slot-1

---
+++ Practice-Questions.py
@@ -0,0 +1,21 @@
+#Ques 1 - Write a program to input 2 numbers and print their sum.
+a = int(input("Enter value of a:"))
+b = int(input("Enter value of b:"))
+sum = a+b
+print("Sum of the numbers is :", sum)
+
+#Ques 2 - Write a program to input side of a square and print its area.
+side = int(input("Enter side of the square:"))
+area = side**2
+print("Area of the square is:", area)
+
+#Ques 3 - Write a program to input 2 floating numbers and print their average.
+f1 = float(input("Enter value of f1:"))
+f2 = float(input("Enter value of f2:"))
+Avg = (f1+f2)/2
+print("The average of the numbers is :", Avg)
+
+#Ques 3 - Write a program to input 2 integer numbers a and b. Print True if a is greater than or equal to b, if not print False
+A = int(input("Enter value of A:"))
+B = int(input("Enter value of B:"))
+print(A>=B)
