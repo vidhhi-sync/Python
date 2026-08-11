e318216a87b801882eb49bf5339d2ff7a2dd226c
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-11 12:58:25 +0530

    Adding Operators

---
+++ Operators.py
@@ -0,0 +1,43 @@
+#Operators
+
+#Arithmetic operators
+d = 4
+e = 2
+print(d+e)
+print(d-e)
+print(d*e)
+print(d/e)
+print(d**e)
+print(d%e)
+
+#Relational Operators
+f = 3
+g = 6
+print(f==g)
+print(f!=g)
+print(f>g)
+print(f<g)
+
+#Assignment Operators
+num = 100
+num+=10 #means num = num + 10, which is 100 + 10
+print(num)
+num-=10
+print(num)
+num*=1
+print(num)
+num/=10
+print(num)
+num%=10
+print(num)
+num**=2
+print(num)
+
+#Logical Operators
+print(not False)
+h = 10
+i = 20
+print(h>i and i>h)
+print(h>i or i>h)
+print(not i>h)
+print(not h>i)
