b2884b738b05113057ffc34db5acb30fdd588dd7
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-11 12:57:53 +0530

    Adding Dataypes

---
+++ Datatypes.py
@@ -0,0 +1,30 @@
+#Data types
+
+#integer datatypes
+a = 12
+b= -12
+c = 0
+print(a,b,c)
+
+#string datatypes
+Name ='vb'
+Name1 = "vi"
+Name2 = '''vidhii'''
+print(Name,Name1,Name2)
+
+#float datatypes
+float1 = 24.33
+print(float1)
+
+#Boolean  datatypes
+A = True
+print(A)
+
+#None datatype
+B = None
+print(B)
+print(type(B))
+print(type(A))
+print(type(float1))
+print(type(Name1))
+print(type(a))
