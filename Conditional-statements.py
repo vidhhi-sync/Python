fb41590e5811d8238db56037ba0701a74eb84373
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-12 12:52:26 +0530

    Adding Conditional Statements

---
+++ Conditional-statements.py
@@ -0,0 +1,16 @@
+#If-elif-else
+a = input("Enter a:")
+b = input("Enter b:")
+if (a==b):
+    print("Equality")
+elif (a>b):
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+
+#eligibilty for license
+age = int(input("Enter age:"))
+if (age>=18):
+    print("Eligible")
+else:
+    print("Not Eligible")
