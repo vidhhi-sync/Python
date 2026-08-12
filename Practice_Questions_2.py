2e6df97b5234a42114fb832fee4de25a5f83d316
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-12 12:46:40 +0530

    Adding Practice Questions Slot-2

---
+++ Practice_Questions_2.py
@@ -0,0 +1,11 @@
+#practice
+
+#Ques-1 Write a program to input user's first name and print it's length
+str = input("Enter your name:")
+print("My name is:",str)
+print(len(str))
+
+#Ques-2 Write a program to find the occurence of $ in string.
+str1 = "The price ($) of the apple juice is: $0.2"
+print(str1)
+print(str1.count("$"))
