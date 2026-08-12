1173afeda7d44af9d9482bad4ecc599dad97a1a5
Vidhi-Bansal <vidhibansal528@gmail.com>
2026-08-12 12:37:47 +0530

    Adding Strings and Strings Functions

--- string.py
+++ string.py
@@ -43,3 +43,44 @@ print(ss)
 #printing from start to a particular index number
 print(strr[:4]) #[0:4]
 print(strr[0:4])
+
+#Negative Indexing in slicing
+string = "Apple"
+print(string[-5:-2])
+
+#String Functions
+
+#str.endswith()
+#this function checks whether our string ends with something particular or not
+b = "I am Vidhi Bansal."
+print(b.endswith(" Bansal."))
+
+#str.capitalize()
+#This function capitalize the first letter of the string
+a = "hi"
+print(a.capitalize())
+#this function works only once , it doesn't change the original string
+print(a)
+#but if we want to change in original string then we can store it in another variable
+c = a.capitalize() #now this changes in original string
+print(c)
+
+#str.replace(old,new)
+#this function is used to replace any specific value to another value
+d = "this is a string"
+print(d.replace("i","e"))
+
+#str.find(word)
+#this function helps in finding a word in a string
+#if the word is present in string then the occurence of that word 1st time prints 1st index number of that word
+e = "This is a string function"
+print(e.find("a"))
+print(e.find("x")) #here it gives negative as it doesn't include in that string so considered invalid
+
+#str.counts()
+#this function helps in counting the number of occurence of that word in a string
+f = "Hi this is vidhi"
+print(f.count("i"))
+print(f.count("h"))
+print(f.count("m"))
+
